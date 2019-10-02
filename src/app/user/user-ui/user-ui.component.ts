0603bf5eabd6981351ce32618b7ac54d526ce197
iamced07 <iamced07@gmail.com>
2019-10-03 04:09:34 +0800

    walang conflict wahawhwah

--- src/app/user/user-ui/user-ui.component.ts
+++ src/app/user/user-ui/user-ui.component.ts
@@ -15,15 +15,38 @@ export class UserUIComponent implements OnInit {
   filterResult = [];
   // tslint:disable-next-line: variable-name
   private _searchTerm: string;
+  KeywordIsDisabled = false;
+  TitleIsDisabled = false;
+  AuthorIsDisabled = false;
+  myTitle = false;
+  myAuthor = false;
+  myKeyword = false;
   constructor(private data: DataService, private User: DataService, private router: Router) { }
   filtersResult(searchString: string) {
     return this.result.filter(res => res.origin.toLowerCase().indexOf(searchString.toLowerCase()) !== -1 );
+   }
+   filtersTitle(searchString: string) {
+    return this.result.filter(res1 => res1.destination.toLowerCase().indexOf(searchString.toLowerCase()) !== -1 );
+   }
+   filtersAuthor(searchString: string) {
+    return this.result.filter(res2 => res2.ship.toLowerCase().indexOf(searchString.toLowerCase()) !== -1 );
    }
 
   onEnter(value: string) {
     this.value = value;
     this.message = this.value;
     this.filterResult = this.filtersResult(value);
+  }
+  onEnter1(value: string) {
+    this.value = value;
+    this.message = this.value;
+    this.filterResult = this.filtersTitle(value);
+  }
+  onEnter2(value: string) {
+    this.value = value;
+    this.message = this.value;
+    this.filterResult = this.filtersTitle(value);
+
   }
   clicked = function() {
     this.click = true;
@@ -35,6 +58,8 @@ export class UserUIComponent implements OnInit {
     this.data.currentMessage.subscribe(message => {
       this.message = message;
       this.filterResult = this.filtersResult(message);
+      this.filterResult = this.filtersTitle(message);
+      this.filterResult = this.filtersAuthor(message);
         });
     this.User.getMessage()
       .subscribe(data => this.result = data);
@@ -42,4 +67,33 @@ export class UserUIComponent implements OnInit {
       .subscribe(data => this.filterResult = data);
      }
 
+     newClick() {
+       this.KeywordIsDisabled = false;
+       this.TitleIsDisabled = false;
+       this.AuthorIsDisabled = false;
+     }
+   KeywordClick() {
+     this.KeywordIsDisabled = false;
+     this.TitleIsDisabled = true;
+     this.AuthorIsDisabled = true;
+     this.myKeyword = true;
+     this.myTitle = false;
+     this.myAuthor = false;
+   }
+   TitleClick() {
+     this.KeywordIsDisabled = true;
+     this.TitleIsDisabled = false;
+     this.AuthorIsDisabled = true;
+     this.myKeyword = false;
+     this.myTitle = true;
+     this.myAuthor = false;
+   }
+   AuthorClick() {
+     this.KeywordIsDisabled = true;
+     this.TitleIsDisabled = true;
+     this.AuthorIsDisabled = false;
+     this.myKeyword = false;
+     this.myTitle = false;
+     this.myAuthor = true;
+   }
 }
